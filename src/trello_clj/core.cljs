8f47c42826ec31d13f43502c2d63019f410f5bc3
Mattias Lundell <capitrane@gmail.com>
2014-10-18 19:46:30 +0200

    basic rendering of posts

--- src/trello_clj/core.cljs
+++ src/trello_clj/core.cljs
@@ -1,5 +1,32 @@
-(ns trello-clj.core)
+(ns trello-clj.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
-(println "Hello world!")
+(def app-state
+  (atom
+   {:posts [{:name "a" :desc "test a"}
+            {:name "b" :desc "test b"}]}))
+
+(defn content [post]
+  (:desc post))
+
+(defn post-view [post owner]
+  (reify
+    om/IRender
+    (render [this]
+      (apply dom/div nil [(dom/h1 nil (:name post))
+                          (dom/p nil (:desc post))]))))
+
+(defn posts-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h1 nil "Posts")
+               (apply dom/div nil
+                      (om/build-all post-view (:posts app)))))))
+
+(om/root posts-view app-state
+         {:target (. js/document (getElementById "app"))})
