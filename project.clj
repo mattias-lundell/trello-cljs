c39eca84d932be09eeace11944970c36c1899b8c
Mattias Lundell <mattias@lundell.com>
2014-10-20 22:05:35 +0200

    added markdown to html conversation of post body

--- project.clj
+++ project.clj
@@ -5,13 +5,14 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2311"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.7.1"]]
+                 [om "0.7.1"]
+                 [markdown-clj "0.9.55"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "trello-clj"
               :source-paths ["src"]
               :compiler {
--- src/trello_clj/core.cljs
+++ src/trello_clj/core.cljs
@@ -1,23 +1,28 @@
 (ns trello-clj.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [markdown.core :refer [md->html]]
+            ))
 
 (enable-console-print!)
 
 (def app-state
   (atom
-   {:posts [{:name "a" :desc "test a"}
-            {:name "b" :desc "test b"}]}))
+   {:posts [{:name "a" :desc "---\ntest\n---"}
+            {:name "b" :desc "- test b"}]}))
+
+(defn title [post]
+  (dom/h1 nil (:name post)))
 
 (defn content [post]
-  (:desc post))
+  (let [html (md->html (:desc post))]
+    (dom/div #js {:dangerouslySetInnerHTML #js {:__html html}} nil)))
 
 (defn post-view [post owner]
   (reify
     om/IRender
     (render [this]
-      (apply dom/div nil [(dom/h1 nil (:name post))
-                          (dom/p nil (:desc post))]))))
+      (apply dom/div nil [(title post) (content post)]))))
 
 (defn posts-view [app owner]
   (reify
